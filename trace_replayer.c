47e3319661a2e43e4e863e4fe1d0bc4128c4583c
ChoiMoonSeok <mon08290@naver.com>
2025-02-21 09:07:04 +0000

    change nvme_write to nvme_ns_write

--- trace_replayer.c
+++ trace_replayer.c
@@ -8,75 +8,80 @@
 #define NUM_OF_GROUP 6
 
 
-int submit_io_host(int fd, long long int lba, int size, int dt){
+int submit_io_host(nvme_ns_t n, long long int lba, int size, int dt){
 
-    int *data = malloc(sizeof(int) * size / 4); 
+    int *data = malloc(size); 
 
-    struct nvme_io_args args = {
-		.args_size = sizeof(args),
-		.fd = fd,
-		.nsid = 1,
-        .nlb = 1,
-		.control = 0,
-		.dsm = 0,
-		.dspec = 0,
-		.reftag = 0,
-		.apptag = 0,
-		.appmask = 0,
-		.storage_tag = 0,
-		.data_len = sizeof(int) * size / 4,
-		.data = data,
-	 	.metadata_len = 0,
-		.metadata = NULL,
-		.timeout = NVME_DEFAULT_IOCTL_TIMEOUT,
-		.result = NULL,
-	};
-	// printf("%lld %d dt %d\n", lba, size, dt);
-	if (dt > -1)
-		args.slba = (uint64_t)(lba * 10 + dt);
-	else
-		args.slba = (uint64_t)(lba * 10 + NUM_OF_GROUP - 1);
-
-	// args.slba = (uint64_t)10000;
-	
-	// printf("%lld %d\n", lba * 10 + NUM_OF_GROUP - 1, args.data_len);
-	nvme_write(&args);
-	nvme_flush(fd, 1);
+	if (dt > -1){
+		int ret = nvme_ns_write(n, data, (lba * 10 + dt) * 512, size);
+		// printf("%d %lld %d\n", size / 512, ((lba * 10 + dt) * 512) & 511, (size / 512) & 511);
+		if (ret == -1)
+			return -1;
+	}
+	else{
+		int ret = nvme_ns_write(n, data, (lba * 10 + NUM_OF_GROUP - 1) * 512, size);
+		if (ret == -1)
+			return -1;
+	}
+	nvme_ns_flush(n);
 
 	free(data);
 
 	return 0;
 }
 
+
 int main(){
 
-	int fd;
+	nvme_root_t r;
+	nvme_host_t h;
+	nvme_subsystem_t s, _s;
+	nvme_ctrl_t c, _c;
+	nvme_path_t p, _p;
+	nvme_ns_t n, _n;
 	FILE *file;
 
+	r = nvme_scan(NULL);
+	if (!r)
+		return -1;
 
-	fd = open("/dev/nvme0n1", O_RDWR);
-    if (fd == -1){
-		printf("nvme open fail\n");
-		exit(1);
-	}
+	h = nvme_first_host(r);
+	s = nvme_first_subsystem(h);
+	c = nvme_subsystem_first_ctrl(s);
+	n = nvme_ctrl_first_ns(c);
+
+	printf("%s\n", nvme_subsystem_get_name(s));
+	printf("%s\n", nvme_subsystem_get_nqn(s));
+	printf("%s\n", nvme_ctrl_get_name(c));
+	printf("%s\n", nvme_ns_get_name(n));
 
     file = file_open("modified_hm_0.csv");
 
 	printf("I/O replay start\n");
-    for (int i = 0; i < 1; i++){
+    
+    int io_cnt = 0;
+    while (1){
 
+		int ret = 0;
         long long int *line = read_line(file);
         if (line != NULL){
-			submit_io_host(fd, line[0], (int)line[1], (int)line[2]);
+			ret = submit_io_host(n, line[0], (int)line[1], (int)line[2]);
             free(line);
         }
         else
             break;
+
+		if (ret < 0)
+			printf("nvme_ns_write fail\n");
+
+        if (io_cnt % 1000 == 0)
+	    	nvme_ns_flush(n);
     }
 
     printf("end\n");
 
     fclose(file);
+	nvme_free_tree(r);
 
     return 0;
 }
